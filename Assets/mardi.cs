927aa88597bdb54b5b7aabe79525170d4e5b0943
ValentinKajdan <valentin.kajdan@orange.fr>
2017-04-03 15:25:40 +0200

    init

---
+++ Assets/mardi.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mardi : MonoBehaviour {
+
+	private Rigidbody myRigidBody;
+	public float jumpValue;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Left ();
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Jump ();
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Left ();
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			Right ();
+		}
+	}
+
+	public void Jump () {
+		myRigidBody.AddForce (200f * Vector3.up);
+	}
+	public void Left () {
+		myRigidBody.AddForce (30f * Vector3.left);
+	}
+	public void Right () {
+		myRigidBody.AddForce (200f * Vector3.right);
+	}
+
+	void FixedUpdate () {
+
+	}
+
+	void OnTriggerEnter() {
+
+	}
+}
